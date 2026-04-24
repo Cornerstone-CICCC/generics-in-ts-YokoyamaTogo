0172e9d0382fd0ed72e03e2b66ed83bd693172bf
kirakira <15catinthebox@gmail.com>
2026-04-23 20:21:15 -0700

    ex06

--- exercises/ex06.ts
+++ exercises/ex06.ts
@@ -1,6 +1,9 @@
 // Define a generic function named `createPair` that takes two arguments of types `A` and `B` and returns them as a pair within an array `[A, B]`.
 // Provide test cases using different types for `A` and `B` parameters.
 
-const createPair = () => {
-  // Code here
-};
+const createPair = <A, B>(firstValue: A, secondValue: B): [A, B] => {
+  return [firstValue, secondValue]
+}
+
+console.log(createPair("age", 25))
+console.log(createPair(true, "active"))
