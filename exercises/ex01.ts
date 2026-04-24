3da76d73759876f19a6e508361a7ee727e7ed99c
kirakira <15catinthebox@gmail.com>
2026-04-23 20:20:49 -0700

    ex01

--- exercises/ex01.ts
+++ exercises/ex01.ts
@@ -2,6 +2,23 @@
 // These arguments could be strings or numbers. If both are strings, use the concat method; otherwise, just sum the numbers.
 // If they are not of the same type, print an error message to the console; If they are not string or number, print an error message
 
-const concatSum = () => {
-  //Code here
-};
+const concatSum = <T extends string | number>(firstValue: T, secondValue: T): string | number | void => {
+  if (typeof firstValue !== typeof secondValue) {
+    console.error("Both arguments must be the same type")
+    return
+  }
+
+  if (typeof firstValue === "string" && typeof secondValue === "string") {
+    return firstValue.concat(secondValue)
+  }
+
+  if (typeof firstValue === "number" && typeof secondValue === "number") {
+    return firstValue + secondValue
+  }
+
+  console.error("Arguments must be strings or numbers")
+}
+
+console.log(concatSum("Hello, ", "TypeScript"))
+console.log(concatSum(10, 20))
+console.log(concatSum(true as unknown as string, false as unknown as string))
