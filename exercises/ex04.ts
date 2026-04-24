48362e326351130c7827536d42f227e2e268a952
kirakira <15catinthebox@gmail.com>
2026-04-23 20:21:07 -0700

    ex04

--- exercises/ex04.ts
+++ exercises/ex04.ts
@@ -2,6 +2,11 @@
 // Create test cases using arrays of various types to demonstrate that the function works correctly.
 // Don't forget to cover the situation where the array is empty.
 
-const getFirstElement = () => {
-  // Code here
-};
+const getFirstElement = <T>(array: T[]): T | undefined => {
+  return array[0]
+}
+
+console.log(getFirstElement([10, 20, 30]))
+console.log(getFirstElement(["apple", "banana", "orange"]))
+console.log(getFirstElement([{ id: 1, name: "Togo" }, { id: 2, name: "Yuki" }]))
+console.log(getFirstElement([]))
