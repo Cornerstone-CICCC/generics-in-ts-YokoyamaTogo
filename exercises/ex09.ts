ed7ffea0da5e129cdeb766690a28d1d3f7697773
kirakira <15catinthebox@gmail.com>
2026-04-23 20:21:29 -0700

    ex09

--- exercises/ex09.ts
+++ exercises/ex09.ts
@@ -1,3 +1,28 @@
 // Define a generic class named `Stack` that implements a stack data structure for elements of type `T`.
 // Implement methods for pushing and popping elements from the stack.
 // Create test cases using various types for elements pushed and popped from the stack.
+
+class Stack<T> {
+  private items: T[] = []
+
+  push(item: T): void {
+    this.items.push(item)
+  }
+
+  pop(): T | undefined {
+    return this.items.pop()
+  }
+}
+
+const numberStack = new Stack<number>()
+numberStack.push(10)
+numberStack.push(20)
+
+console.log(numberStack.pop())
+console.log(numberStack.pop())
+
+const stringStack = new Stack<string>()
+stringStack.push("first")
+stringStack.push("second")
+
+console.log(stringStack.pop())
