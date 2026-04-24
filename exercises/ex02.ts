a8657bc4b4d97569fb269ba1ef06260a483779a0
kirakira <15catinthebox@gmail.com>
2026-04-23 20:20:59 -0700

    ex02

--- exercises/ex02.ts
+++ exercises/ex02.ts
@@ -1,6 +1,14 @@
 // Create a generic function named `identity` that takes a single argument of type `T` and returns that argument.
 // Create a few test cases, such as using the function with numbers, strings, and custom objects to demonstrate its type flexibility.
 
-const identify = () => {
-  // Code here
-};
+const identity = <T>(value: T): T => {
+  return value
+}
+
+const numberIdentity = identity(25)
+const stringIdentity = identity("TypeScript")
+const objectIdentity = identity({ id: 1, name: "Togo" })
+
+console.log(numberIdentity)
+console.log(stringIdentity)
+console.log(objectIdentity)
